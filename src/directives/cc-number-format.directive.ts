ce88aeef91ea9989c9c864542e82750f5f38ce04
Wesklei Migliorini <wesklei.m@gmail.com>
2020-12-09 17:28:27 -0300

    migrate from Renderer to Renderer2

--- src/directives/cc-number-format.directive.ts
+++ src/directives/cc-number-format.directive.ts
@@ -1,20 +1,20 @@
-import { 
-	Directive, 
-	ElementRef, 
-	Renderer,
+import {
+	Directive,
+	ElementRef,
+	Renderer2,
 	HostListener
 } from '@angular/core';
 
 import * as Payment from 'payment';
 
-@Directive({ 
-	selector: '[ccNum]' 
+@Directive({
+	selector: '[ccNum]'
 })
 export class CCNumberFormatDirective {
 
 	cardType: string;
 
-	constructor(private renderer: Renderer, private el: ElementRef) {
+	constructor(private renderer2: Renderer2, private el: ElementRef) {
 
 		let element   = this.el.nativeElement;
 		this.cardType = "";
@@ -25,19 +25,19 @@ export class CCNumberFormatDirective {
 	}
 
 	@HostListener('keypress', ['$event']) onKeypress(e) {
-		
+
 		let element 	  = this.el.nativeElement;
 		let elementValue  = element.value;
 
 		this.cardType = Payment.fns.cardType(elementValue);
 
 		if ( this.cardType !== "" )
-		{	
-			this.renderer.setElementClass(element, this.cardType, false);
+		{
+      this.renderer2.removeClass(element, this.cardType);
 		}
 		else
 		{
-			this.cardType = "";			
+			this.cardType = "";
 		}
 
 	}
